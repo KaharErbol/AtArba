149c5a8f778376d5403ff24d90cfb6248e23249b
Kahar Erbol <kaharerbol@gmail.com>
2023-05-02 18:15:54 -0700

    added signup front end feature, now list item, then fix the cart issue

--- ghi/src/loginForm.js
+++ ghi/src/loginForm.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { useLoginMutation } from "./store/authApi";
+import { useLoginMutation, useSignUpMutation } from "./store/authApi";
 import { useNavigate } from "react-router-dom";
 
 
@@ -59,6 +59,22 @@ function LoginForm({ token }) {
     const [phone, setPhone] = useState('');
     const [zip, setZip] = useState('');
     const [avatarUrl, setAvatarUrl] = useState('');
+    const [signup, resultSignup] = useSignUpMutation();
+
+    const handleSignupSubmit = async (e) => {
+        e.preventDefault();
+        await signup({
+            "first_name": firstName,
+            "last_name": lastName,
+            "username": username,
+            "email": email,
+            "phone": phone,
+            "zip": zip,
+            "password": password,
+            "avatar_url": avatarUrl,
+        });
+        e.target.reset();
+    }
 
 
     return (
@@ -66,7 +82,7 @@ function LoginForm({ token }) {
         <div className="body-login">
         <div className={`container ${containerClass}`} id="container">
             <div className="form-container sign-up-container">
-                <form action="#">
+                <form onSubmit={handleSignupSubmit} >
                     <h1>Create Account</h1>
                     {/* <div className="social-container">
                         <a href="#" className="social"><i className="fab fa-facebook-f"></i></a>
@@ -74,15 +90,15 @@ function LoginForm({ token }) {
                         <a href="#" className="social"><i className="fab fa-linkedin-in"></i></a>
                     </div> */}
                     <span>input your info for registration</span>
-                    <input type="text" placeholder="First Name" />
-                    <input type="text" placeholder="Last Name" />
-                    <input type="text" placeholder="Username" />
-                    <input type="email" placeholder="Email" />
-                    <input type="text" placeholder="Phone" />
-                    <input type="text" placeholder="Zip Code" />
-                    <input type="password" placeholder="Password" />
-                    <input type="text" placeholder="Avatar Url" />
-                    <button>Sign Up</button>
+                    <input type="text" placeholder="First Name" name='first_name' id='first_name' value={firstName} onChange={ e => setFirstName(e.target.value)} />
+                    <input type="text" placeholder="Last Name" name='last_name' id='last_name' value={lastName} onChange={ e => setLastName(e.target.value)} />
+                    <input type="text" placeholder="Username" name='username' id='username' value={username} onChange={ e => setUsername(e.target.value)} />
+                    <input type="email" placeholder="Email" name='email' id='email' value={email} onChange={ e => setEmail(e.target.value)} />
+                    <input type="text" placeholder="Phone" name='phone' id='phone' value={phone} onChange={ e => setPhone(e.target.value)} />
+                    <input type="text" placeholder="Zip Code" name='zip' id='zip' value={zip} onChange={ e => setZip(e.target.value)} />
+                    <input type="password" placeholder="Password" name='password' id='password' value={password} onChange={ e => setPassword(e.target.value)}/>
+                    <input type="text" placeholder="Avatar Url" name='avatar' id='avatar' value={avatarUrl} onChange={ e => setAvatarUrl(e.target.value)} />
+                    <button type='submit' >Sign Up</button>
                 </form>
 	        </div>
             <div className="form-container sign-in-container">
